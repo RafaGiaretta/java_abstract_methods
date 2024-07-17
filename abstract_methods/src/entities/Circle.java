10ccadb9a7dc79c7677fefb26f296258f2af9343
RafaGiaretta <rafaelgiaretta@ymail.com>
2024-07-17 00:57:12 -0300

    FIRST COMMIT

---
+++ abstract_methods/src/entities/Circle.java
@@ -0,0 +1,25 @@
+package entities;
+
+public class Circle extends Shape {
+
+	private Double radius;
+
+	public Circle(Color color, Double radius) {
+		super(color);
+		this.radius = radius;
+	}
+
+	public Double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(Double radius) {
+		this.radius = radius;
+	}
+
+	@Override
+	public double area() {
+		return Math.pow(radius,2) * Math.PI;
+	}
+
+}
